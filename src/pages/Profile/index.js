b8fc29907588cd2bb3e60ff5fc4afb4df865f290
DevRafaelDeMelo <rafaelmelo765@gmail.com>
2023-11-15 17:04:40 -0300

    Pagina inicial

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,10 +1,50 @@
-import {} from "./styles"
-
+import { Perfil, CardStatus, Box, BarNvl, HypeLinks } from "./styles";
+import ImgProfile from "../../assets/Perfil.png";
+import { BsCash, BsPerson, BsBell, BsShieldCheck } from "react-icons/bs";
 
 export const Profile = () => {
-    return(<>
-        <h1> Profile</h1>
-    </>)
-
-
-}
+  return (
+    <Perfil>
+      <h1>Perfil</h1>
+      <Box>
+        <img src={ImgProfile} />
+        <h2>João Perreira</h2>
+        <h3>Joao@perreira.com.br</h3>
+        <h4>Nivel 5</h4>
+        <BarNvl>
+          <div></div>
+        </BarNvl>
+        <CardStatus>
+          <div>
+            <h1>Cortes</h1>
+            <h2>89</h2>
+          </div>
+          <div>
+            <h1>Notas</h1>
+            <h2>
+              7,8<span>/10</span>
+            </h2>
+          </div>
+        </CardStatus>
+      </Box>
+      <HypeLinks>
+        <div>
+          <BsPerson />
+          <h1>Editar perfil</h1>
+        </div>
+        <div>
+          <BsBell />
+          <h1>Notificações</h1>
+        </div>
+        <div>
+          <BsCash />
+          <h1>Pagamentos</h1>
+        </div>
+        <div>
+          <BsShieldCheck />
+          <h1>Segurança</h1>
+        </div>
+      </HypeLinks>
+    </Perfil>
+  );
+};
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,2 +1,112 @@
 import styled from "styled-components";
+import { Themes } from "../../styles/themes";
 
+export const Perfil = styled.div`
+  padding-top: 10vh;
+  h1 {
+    padding-top: 10px;
+    text-align: center;
+    color: ${Themes.yellow};
+    font-size: 20px;
+  }
+`;
+
+export const Box = styled.div`
+  background: #272727;
+  width: 90%;
+  margin: auto;
+  border-radius: 20px;
+  text-align: center;
+  margin-top: 20px;
+  padding-bottom: 20px;
+  img {
+    margin-top: 20px;
+    height: 100px;
+    width: 100px;
+    background: red;
+    background-size: cover;
+    border-radius: 100%;
+    border: 5px solid white;
+  }
+  h2 {
+    color: white;
+    font-size: 15px;
+    font-weight: 300;
+  }
+  h3 {
+    color: ${Themes.yellow};
+    font-size: 15px;
+  }
+  h4 {
+    margin-top: 20px;
+    color: white;
+  }
+  section {
+    width: 25%;
+    background: white;
+    height: 100px;
+  }
+`;
+
+export const CardStatus = styled.div`
+  display: flex;
+  width: 80%;
+  margin: auto;
+  margin-top: 20px;
+  div {
+    border-radius: 10px;
+    background: white;
+    width: 30%;
+    justify-content: space-between;
+    margin: auto;
+    padding: 10px 0px 10px 0px;
+    h1 {
+      font-size: 13px;
+      color: black;
+      padding: 0%;
+    }
+    h2 {
+      color: ${Themes.yellow};
+      font-size: 20px;
+      font-weight: 800;
+      span {
+        font-size: 10px;
+      }
+    }
+  }
+`;
+
+export const BarNvl = styled.div`
+  background: #565656;
+  width: 50%;
+  margin: auto;
+  margin-top: 5px;
+  border-radius: 100px;
+  height: 10px;
+  div {
+    border-radius: 100px;
+    height: 10px;
+    width: 40%;
+    background: ${Themes.yellow};
+  }
+`;
+
+export const HypeLinks = styled.div`
+  color: white;
+  padding: 30px;
+  svg {
+    height: 25px;
+    width: 25px;
+    margin-right: 40px;
+  }
+  h1 {
+    color: white;
+    font-size: 15px;
+    font-weight: 300;
+    padding: 0px;
+  }
+  div {
+    margin-top: 20px;
+    display: flex;
+  }
+`;
